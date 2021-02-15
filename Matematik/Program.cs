90ea714ea572e4d30ec44863b693bab6706d6863
egemen <egemennbalci@gmail.com>
2021-02-15 20:10:58 +0300

    aa

--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -7,7 +7,7 @@ namespace Matematik
         static void Main(string[] args)
         {
             DortIslem dortIslem = new DortIslem();
-            dortIslem.Topla(5, 6);
+            dortIslem.Topla(5, 10);
 
             dortIslem.Topla(6, 9);
         }
